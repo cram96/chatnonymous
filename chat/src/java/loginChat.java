023c0361275fbb35fc46557860555fe00d6b8d4f
chuki <chuki@miguelpc>
2018-11-04 18:37:41 +0100

    Creación del repo, y del login (no es perfecto)

---
+++ chat/src/java/loginChat.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author chuki
+ */
+public class loginChat extends HttpServlet {
+        String usuario_valido = "chuki";
+        String contrasena_valido="1234";
+        
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                PrintWriter out;
+            out = response.getWriter();
+        response.setContentType("text/html;charset=UTF-8");
+        String usuario =request.getParameter("username");
+        String contrasena = request.getParameter("password");
+        if (usuario.equals(usuario_valido) && contrasena.equals(contrasena_valido)){
+
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet logeado</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> Logeado correctamente " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        
+        }
+        else{
+             out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet logeado</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> Error al autentificarse</h1>");
+            out.println("<a href=\"http://localhost:8080/Chat\">Volver</a><br/>");
+            out.println("</body>");
+            out.println("</html>");
+        
+        }
+        }
+        
+        
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
